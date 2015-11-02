f5935fc5b043f2e8ab846e456b1c37f80d109bdf
Hurricane Hunia <hurricane@kotukusystems.com>
2015-11-02 20:20:22 +1300

    Individual apparel group page developed further, and some site changes

---
+++ static/javascript/main.js
@@ -0,0 +1,18 @@
+﻿$(document).ready(function () {
+
+    // Contact
+    $('#send').click(function () {
+        $.ajax({
+            url: $(".frm").attr('action'),
+            method: 'POST',
+            data: $('.frm').serialize(),
+            dataType: 'json',
+            beforeSend: function () { $('#send').attr('value', 'Message sending.....'); },
+            success: function (data) { $('#send').attr('value', 'Message sent successfully'); },
+            error: function (err) { $('#send').attr('value', 'There was an error!'); }
+        });
+        return false;
+    });
+
+});
+     
