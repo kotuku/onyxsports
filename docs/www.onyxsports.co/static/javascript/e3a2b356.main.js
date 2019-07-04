38f7120e05fac1be25a1bdb0e258f42057e736e9
HH <hurricane@kotukusystems.com>
2019-07-05 09:14:52 +1200

    Static Site

---
+++ docs/www.onyxsports.co/static/javascript/e3a2b356.main.js
@@ -0,0 +1,35 @@
+﻿$(document).ready(function () {
+
+    // When the user clicks the toTop button, we want the page to scroll to the top.
+    $("#toTop").click(function () {
+
+        // Disable the default behaviour when a user clicks an empty anchor link.
+        // (The page jumps to the top instead of // animating)
+        event.preventDefault();
+
+        // Animate the scrolling motion.
+        $("html, body").animate({
+            scrollTop: 0
+        }, "slow");
+
+    });
+
+    // Contact
+    $('#frm').submit(function(e) {
+        e.preventDefault();
+        $.ajax({
+            url: $("#frm").attr('action'),
+            method: 'POST',
+            data: $("#frm").serialize(),
+            dataType: 'json',
+            beforeSend: function () { $('#send').attr('value', 'Message sending.....'); },
+            success: function (data) {
+                $('#send').attr('value', 'Message sent successfully');
+                dataLayer.push({ 'event': 'formSubmitted', 'frm': 'Contact Us' });
+            },
+            error: function (err) { $('#send').attr('value', 'There was an error!'); }
+        });
+    });
+
+});
+     
